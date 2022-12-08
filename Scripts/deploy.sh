d187f3d1dc755ca6adc91c16d69a7f5598cfe30d
GedFlow <upmoo1327@gmail.com>
2022-12-08 12:56:21 +0900

    debug and test 5

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -15,7 +15,6 @@ sudo make altinstall
 
 sudo python3.9 -m pip install --upgrade pip
 
-cd -
-cd qrqr_backend
+cd /home/ubuntu/code_pipeline/qrqr_backend
 pip3.9 install -r requirements.txt
 gunicorn --bind=0.0.0.0:8000 config.wsgi:application
