53d7f3fa2985ba84ec140dd798cdc7760d6554d6
GedFlow <115227099+GedFlow@users.noreply.github.com>
2022-12-09 16:43:25 +0900

    add defaultFile in deploy.sh

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt -y install nginx
+
+sudo wget https://qrqr-static.s3.ap-northeast-2.amazonaws.com/default
+sudo \mv -f default /etc/nginx/sites-available/default
+
 sudo systemctl restart nginx
