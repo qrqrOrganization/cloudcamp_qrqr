33454fb78575ced7b7a6fb08cf96aa952339a1b8
GedFlow <upmoo1327@gmail.com>
2022-12-10 00:02:36 +0900

    debug and test 2

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt-get install build-essential checkinstall
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
-    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+# sudo apt-get install build-essential checkinstall
+# sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+#     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 
-cd /opt
-sudo wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
-sudo tar xzf Python-3.9.1.tgz
+# cd /opt
+# sudo wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
+# sudo tar xzf Python-3.9.1.tgz
 
-cd Python-3.9.1
-sudo ./configure --enable-optimizations
-sudo make altinstall
+# cd Python-3.9.1
+# sudo ./configure --enable-optimizations
+# sudo make altinstall
 
 sudo python3.9 -m pip install --upgrade pip
 
 cd /home/ubuntu/code_pipeline/qrqr_backend
-pip3.9 install -r requirements.txt
 
-sudo apt install awscli
+pip3.9 install -r requirements.txt
 
 sudo aws s3 cp s3://qrqr-config/secret.json ./
 
