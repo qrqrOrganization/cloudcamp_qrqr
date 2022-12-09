117de9576aed75e467544f8694fcf7327e6862ea
GedFlow <upmoo1327@gmail.com>
2022-12-09 20:26:21 +0900

    debug and test 3

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock*
 sudo apt update -y
 sudo apt -y install nginx
 
-sudo apt install awscli -y
-sudo aws s3 cp s3://qrqr-config/default ./
+apt install awscli -y
+aws s3 cp s3://qrqr-config/default ./
 sudo \mv -f default /etc/nginx/sites-available/default
 
 sudo systemctl restart nginx
