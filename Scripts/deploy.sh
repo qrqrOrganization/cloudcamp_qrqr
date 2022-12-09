5ed3298a68de60767ea3ac656f9a9232c1945920
GedFlow <upmoo1327@gmail.com>
2022-12-09 17:30:32 +0900

    change download file to awscli

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -2,7 +2,8 @@
 sudo apt update -y
 sudo apt -y install nginx
 
-sudo wget https://qrqr-static.s3.ap-northeast-2.amazonaws.com/default
+suto apt install awscli
+sudo aws s3 cp s3://qrqr-config/default ./
 sudo \mv -f default /etc/nginx/sites-available/default
 
 sudo systemctl restart nginx
