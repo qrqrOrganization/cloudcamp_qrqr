b30d401b2fd7e3344875ff3c1987451de15ddf10
rhkzl <rhkzl6@gmail.com>
2022-12-10 13:43:49 +0900

    나의 구독, 마이페이지 수정

--- qrqr_backend/config/settings.py
+++ qrqr_backend/config/settings.py
@@ -128,6 +128,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
 
 
 # AWS Setting
--- qrqr_backend/config/urls.py
+++ qrqr_backend/config/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path('unlike/<int:pid>',guduck.views.unlike),
     #구독관리
     path('like/<int:pid>',product.views.like),
+    #path('<int:pid>/guduck/', guduck.views.prog, name='likes'),
+        #상품관리
     #메인페이지
     path('', user.views.mainFunc),
     #상품 #리스트
@@ -21,8 +23,16 @@ urlpatterns = [
     #상세
     path('product/<str:type>', product.views.prog),
     #회원
+    # 가입
     path('signup/', user.views.signup, name="signup"),
     path('login/', user.views.login, name="login"),
     path('logout/', user.views.logout, name="logout"),
     path('mypage/', user.views.mypage, name="mypage"),
+    #구독 관리 guduck
+        # 나의 구독 내역 list
+ #   path('guduck/', product.views.list),
+        # 나의 구독 수정(삭제)
+ #   path('guduck/prog/<str:type>', product.views.prog),
+
+
 ]
