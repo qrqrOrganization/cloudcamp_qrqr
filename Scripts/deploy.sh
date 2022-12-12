a19a64900800d741ec3fa740d19fbd302bae574a
GedFlow <upmoo1327@gmail.com>
2022-12-12 17:10:32 +0900

    may be It's complete!

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -8,5 +8,5 @@
 cd /home/ubuntu/code_pipeline/qrqr_backend
 pip3.9 install -r requirements.txt
 sudo aws s3 cp s3://qrqr-config/secret.json ./
-sudo aws s3 cp ./static/* s3://qrqr-static/static/
+sudo aws s3 cp ./static/ s3://qrqr-static/static/ --recursive
 gunicorn --bind=0.0.0.0:8000 config.wsgi:application --daemon
