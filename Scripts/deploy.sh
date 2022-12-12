38a10a0d9b15e6726d7b3ae0f0862ad38bbb06f6
GedFlow <115227099+GedFlow@users.noreply.github.com>
2022-12-12 12:17:24 +0900

    zoosuck

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo rm /var/lib/apt/lists/lock
-sudo rm /var/cache/apt/archives/lock
-sudo rm /var/lib/dpkg/lock*
+# sudo rm /var/lib/apt/lists/lock
+# sudo rm /var/cache/apt/archives/lock
+# sudo rm /var/lib/dpkg/lock*
 
 sudo apt update -y
 sudo apt -y install nginx
