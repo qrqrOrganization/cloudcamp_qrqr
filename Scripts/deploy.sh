b6d90f3f0689bb161a489e4a2c37eca092e73bba
GedFlow <115227099+GedFlow@users.noreply.github.com>
2022-12-13 10:07:19 +0900

    debug1

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -2,6 +2,6 @@
 sudo aws s3 cp s3://qrqr-config/default ./
 sudo \mv -f default /etc/nginx/sites-available/default
 
-sudo apt purge libnginx-mod-http-perl
+# sudo apt purge libnginx-mod-http-perl
 
 sudo systemctl restart nginx
